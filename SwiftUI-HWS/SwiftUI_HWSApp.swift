d8461d32942b0d409bf8e8aa63e060b1d54c55e3
Prateek Roy <prateekroy01@yahoo.com>
2020-09-25 20:01:30 +0530

    Initial Commit

---
+++ SwiftUI-HWS/SwiftUI_HWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HWSApp.swift
+//  SwiftUI-HWS
+//
+//  Created by Prateek Roy on 25/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
